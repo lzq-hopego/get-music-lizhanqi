c3619efeffb3c042d8c1b21640e1e5d68fec6a3c
lizhanqi <94416555+lzq-hopego@users.noreply.github.com>
2022-04-12 18:01:43 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="get-music-lizhanqi",
+    version="0.0.20",
+    author="Example Author",
+    author_email="3101978435@qq.com",
+    description="可以下载音乐的包哦",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    project_urls={
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "tests"},
+    packages=setuptools.find_packages(where="tests"),
+    python_requires=">=3.5",
+    install_requires=["requests"],
+    entry_points={
+        'console_scripts': ["get-music=get_music.get_music:main"
+            ],
+    },
+)
