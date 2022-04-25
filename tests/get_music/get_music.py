1d1f258be45a419bba009c1a789774f8f0827b60
lizhanqi <94416555+lzq-hopego@users.noreply.github.com>
2022-04-25 17:53:44 +0800

    Update get_music.py

--- tests/get_music/get_music.py
+++ tests/get_music/get_music.py
@@ -1,13 +1,6 @@
-import requests,urllib.parse,json,sys,time,os
+import requests,urllib.parse,json,sys
 from get_music import download
 
-##检测当前目录下是否存在“音乐”文件夹
-##a=os.path.exists('音乐')
-##b=os.getcwd()
-##if a==False:
-##    os.mkdir(b+'//音乐')
-##else:
-##    pass
 
 
 
@@ -26,24 +19,29 @@ def zhuti(Songname=False):
         
         if fs in api.keys():
             if fs==1:
-                song_name,singer_name,song_url=kugou(song)
-                if song_name=='':
-                    print('\n\n\n——很抱歉，我们在接口为'+api[fs]+'的渠道中没有找到'+song+'这首歌。\n\n\n')
+                try:
+                    song_name,singer_name,song_url=netease(song)
+                except:
+                    print("\n`*>﹏<*′\t很抱歉呢，我们无法在此接口找到您想听的歌曲，请核实一下是不是名字错了呀。")
                     sys.exit()
             if fs==2:
-                song_name,singer_name,song_url=netease(song)
-                if song_name=='':
-                    print('\n\n\n——很抱歉，我们在接口为'+api[fs]+'的渠道中没有找到'+song+'这首歌。\n\n\n')
+                try:
+                    song_name,singer_name,song_url=netease(song)
+                except:
+                    print("\n`*>﹏<*′\t很抱歉呢，我们无法在此接口找到您想听的歌曲，请核实一下是不是名字错了呀。")
                     sys.exit()
+                
             if fs==3:
-                song_name,singer_name,song_url=qq(song)
-                if song_name=='':
-                    print('\n\n\n——很抱歉，我们在接口为'+api[fs]+'的渠道中没有找到'+song+'这首歌。\n\n\n')
+                try:
+                    song_name,singer_name,song_url=netease(song)
+                except:
+                    print("\n`*>﹏<*′\t很抱歉呢，我们无法在此接口找到您想听的歌曲，请核实一下是不是名字错了呀。")
                     sys.exit()
             if fs==4:
-                song_name,singer_name,song_url=kuwo(song)
-                if song_name=='':
-                    print('\n\n\n——很抱歉，我们在接口为'+api[fs]+'的渠道中没有找到'+song+'这首歌。\n\n\n')
+                try:
+                    song_name,singer_name,song_url=netease(song)
+                except:
+                    print("\n`*>﹏<*′\t很抱歉呢，我们无法在此接口找到您想听的歌曲，请核实一下是不是名字错了呀。")
                     sys.exit()
             if fs==5:
                 
@@ -88,22 +86,25 @@ def baidu(song):
     baidu.baidu(song)
 ##    migu.migu(song)
 def xiazai(name,singer,song_url):
-    for i in range(0,len(name)-1):
-        print("序号{}\t\t{}————{}".format(i+1,name[i],singer[i]))
-    songs=input('请选择您要下载哪一首歌，直接输入序号就行\n如需下载多个请用逗号分割即可，例如1,2\n如果不需要下载多个，请直接输入序号就行：')
-    if songs=='':
-        print('\n\n\n——您未做出选择！程序将在2秒后自动退出！！！')
-        time.sleep(2)
+    if len(name)==0:
+        print("\n`*>﹏<*′\t很抱歉呢，我们无法在此接口找到您想听的歌曲，请核实一下是不是名字错了呀。")
         sys.exit()
-    song_list=songs.split(",")
-    for i in song_list:
-        i=int(i)-1
-        fname=name[i]+"-"+singer[i]+".mp3"
-        url=song_url[i]
-        download.download(url,fname)
-        print("\n\n"+singer[i]+'唱的'+name[i]+'下载完成啦！')
-        print("已保存至当前目录下")
-    print('\n≧∀≦\t感谢您对本程序的使用，祝您生活愉快！')
+    else:
+        for i in range(0,len(name)-1):
+            print("序号{}\t\t{}————{}".format(i+1,name[i],singer[i]))
+        songs=input('请选择您要下载哪一首歌，直接输入序号就行\n如需下载多个请用逗号分割即可，例如1,2\n如果不需要下载多个，请直接输入序号就行：')
+        if songs=='':
+            print('\n\n\n——您未做出选择！程序将在2秒后自动退出！！！')
+            sys.exit()
+        song_list=songs.split(",")
+        for i in song_list:
+            i=int(i)-1
+            fname=name[i]+"-"+singer[i]+".mp3"
+            url=song_url[i]
+            download.download(url,fname)
+            print("\n\n"+singer[i]+'唱的'+name[i]+'下载完成啦！')
+            print("已保存至当前目录下")
+        print('\n≧∀≦\t感谢您对本程序的使用，祝您生活愉快！')
 
 def main():
     try:
@@ -120,7 +121,8 @@ def main():
                 print("作者邮箱：3101978435@qq.com")
                 print('\n≧∀≦\t感谢您对本程序的使用，祝您生活愉快！')
             elif sys.argv[1] in ['-version','-v','-V']:
-                print("当前版本为v0.0.26")
+                print("当前版本为v0.0.27")
+            
             else:
                 txt='-help\t查看帮助文档\n-version\t查看当前版本\n关于该脚本的使用：\n 1.在dos命令行（或其他系统的命令行）的随意目录下，敲击“get-music”进入该脚本的启动页面\n 2.一般的第一个步骤会提醒你要下载什么歌曲，这是至于要输入歌曲的就行了\n 3.这个步骤程序会让你选择一个搜索歌曲的接口（平台），直接输入接口的序号即可，关于接口（平台）说明，只有咪咕音乐的部分歌曲支持flac格式的，并且在酷狗等音乐接口（平台）可能会下载到试听的部分（这说明该音乐只有vip才能下载，本脚本暂不支持下载vip歌曲和携带vip账号）——如果用酷狗那就直接输入“1”并按下回车\n 4.这个时候程序就该返回数据了，会有19条（不出意外的情况下，当然也跟接口（平台）的返回的数据有关），同样的输入序号即可下载，可以直接输入“1”进行下载\n 5.1在百度音乐接口（平台）会出现让你确定yes或no的提示，这个提示表示是否下载歌词和封面，目前六个接口中仅有百度接口支持，如何不想下载直接回车（或者输入no）\n 5.2执行下载中会有进度条提示，文件越大音质越好\n关于本程序：\n禁止将本脚本用于商业用途，如产生法律纠纷与本人无关，如有侵权，请联系我删除。\n作者兼维护者邮箱：3101978435@qq.com'
                 print(txt)
