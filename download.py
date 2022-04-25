6be5b3148d41c79c2b1ab22c83fd566074b76476
lizhanqi <94416555+lzq-hopego@users.noreply.github.com>
2022-04-24 19:58:35 -0500

    Add files via upload

---
+++ download.py
@@ -0,0 +1,22 @@
+import requests
+from tqdm import tqdm
+
+
+def download(url: str, fname: str):
+    print()
+    # 用流stream的方式获取url的数据
+    resp = requests.get(url, stream=True)
+    # 拿到文件的长度，并把total初始化为0
+    total = int(resp.headers.get('content-length', 0))
+    # 打开当前目录的fname文件(名字你来传入)
+    # 初始化tqdm，传入总数，文件名等数据，接着就是写入，更新等操作了
+    with open(fname, 'wb') as file, tqdm(
+        desc=fname,
+        total=total,
+        unit='iB',
+        unit_scale=True,
+        unit_divisor=1024,
+    ) as bar:
+        for data in resp.iter_content(chunk_size=1024):
+            size = file.write(data)
+            bar.update(size)
