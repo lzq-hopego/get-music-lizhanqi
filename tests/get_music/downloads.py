7cf79750da35d289a51df16080c9292d858be0ba
lizhanqi <94416555+lzq-hopego@users.noreply.github.com>
2022-05-04 20:05:19 -0500

    Add files via upload

--- tests/get_music/downloads.py
+++ tests/get_music/downloads.py
@@ -16,43 +16,82 @@ def downloads():
         print("正在搜索歌曲信息，注意该模式下我们不会返回任何数据进行交互") 
         for i in ls:
             try:
-                songname,songnum,way=i[0],int(i[1]),i[-1]
+                songname,way=i[0],i[-1]
+                songnum=int(i[1])
+                if way=='kw':
+                    print("正在搜索："+songname)
+                    song_name,singers,song_url=kuwo.kuwo(songname)
+                    print("搜索完成，开始下载...")
+                    for i in range(0,songnum):
+                        fname=song_name[i]+"-"+singers[i]+".mp3"
+                        url=song_url[i]
+                        download.download(url,fname)
+                if way=='kg':
+                    print("正在搜索："+songname)
+                    song_name,singers,song_url=kugou.kugou(songname)
+                    print("搜索完成，开始下载...")
+                    for i in range(0,songnum):
+                        fname=song_name[i]+"-"+singers[i]+".mp3"
+                        url=song_url[i]
+                        download.download(url,fname)
+                if way=='qq':
+                    print("正在搜索："+songname)
+                    song_name,singers,song_url=qq.qq(songname)
+                    print("搜索完成，开始下载...")
+                    for i in range(0,songnum):
+                        fname=song_name[i]+"-"+singers[i]+".mp3"
+                        url=song_url[i]
+                        download.download(url,fname)
+                if way=='wy':
+                    print("正在搜索："+songname)
+                    song_name,singers,song_url=netease.netease(songname)
+                    print("搜索完成，开始下载...")
+                    for i in range(0,songnum):
+                        fname=song_name[i]+"-"+singers[i]+".mp3"
+                        url=song_url[i]
+                        download.download(url,fname)
+            except ValueError:
+                songnum=i[1]
+                if way=='kw':
+                    print("正在搜索："+songname)
+                    song_name,singers,song_url=kuwo.kuwo(songname)
+                    print("搜索完成，开始下载...")
+                    for i,j in enumerate(singers):
+                        if songnum in j:
+                            fname=song_name[i]+"-"+singers[i]+".mp3"
+                            url=song_url[i]
+                            download.download(url,fname)
+                if way=='kg':
+                    print("正在搜索："+songname)
+                    song_name,singers,song_url=kugou.kugou(songname)
+                    print("搜索完成，开始下载...")
+                    for i,j in enumerate(singers):
+                        if songnum in j:
+                            fname=song_name[i]+"-"+singers[i]+".mp3"
+                            url=song_url[i]
+                            download.download(url,fname)
+                if way=='qq':
+                    print("正在搜索："+songname)
+                    song_name,singers,song_url=qq.qq(songname)
+                    print("搜索完成，开始下载...")
+                    for i,j in enumerate(singers):
+                        if songnum in j:
+                            fname=song_name[i]+"-"+singers[i]+".mp3"
+                            url=song_url[i]
+                            download.download(url,fname)
+                if way=='wy':
+                    print("正在搜索："+songname)
+                    song_name,singers,song_url=netease.netease(songname)
+                    print("搜索完成，开始下载...")
+                    for i,j in enumerate(singers):
+                        if songnum in j:
+                            fname=song_name[i]+"-"+singers[i]+".mp3"
+                            url=song_url[i]
+                            download.download(url,fname)
+
             except:
                 print("歌曲名称获取失败")
                 break
-            if way=='kw':
-                print("正在搜索："+songname)
-                song_name,singers,song_url=kuwo.kuwo(songname)
-                print("搜索完成，开始下载...")
-                for i in range(0,songnum):
-                    fname=song_name[i]+"-"+singers[i]+".mp3"
-                    url=song_url[i]
-                    download.download(url,fname)
-            if way=='kg':
-                print("正在搜索："+songname)
-                song_name,singers,song_url=kugou.kugou(songname)
-                print("搜索完成，开始下载...")
-                for i in range(0,songnum):
-                    fname=song_name[i]+"-"+singers[i]+".mp3"
-                    url=song_url[i]
-                    download.download(url,fname)
-            if way=='qq':
-                print("正在搜索："+songname)
-                song_name,singers,song_url=qq.qq(songname)
-                print("搜索完成，开始下载...")
-                for i in range(0,songnum):
-                    fname=song_name[i]+"-"+singers[i]+".mp3"
-                    url=song_url[i]
-                    download.download(url,fname)
-            if way=='wy':
-                print("正在搜索："+songname)
-                song_name,singers,song_url=netease.netease(songname)
-                print("搜索完成，开始下载...")
-                for i in range(0,songnum):
-                    fname=song_name[i]+"-"+singers[i]+".mp3"
-                    url=song_url[i]
-                    download.download(url,fname)
-                    print(singers[i]+"唱的"+song_name[i]+"下载完成！\n\n")
     except FileNotFoundError:
         print("您未创建“get_music.txt”文件")
     except:
