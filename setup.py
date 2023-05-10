faae7627b0ccad2d6a1e12228ea79d2371d9c59c
lizhanqi <94416555+lzq-hopego@users.noreply.github.com>
2023-05-10 21:20:46 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="get-music-lizhanqi",
-    version="1.2.5",
+    version="1.2.6",
     author="Li Zhan Qi",
     author_email="3101978435@qq.com",
     description="可以下载音乐的包哦",
